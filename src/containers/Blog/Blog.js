cc851105f685b2b4206d508d74881e12d5ee5488
Felipe Hermosilla <felipe.hermosilla@edge.com.py>
2020-05-09 12:33:01 -0400

    Handling Errors Locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
       posts : [],
-      selectedPostId: null
+      selectedPostId: null,
+      error: false
     }
 
     componentDidMount () {
@@ -22,8 +23,13 @@ class Blog extends Component {
             author: 'Max'
           }
         });
-        this.setState({posts: updatedPosts})
+        this.setState({posts: updatedPosts});
+      }).
+      catch( error => {
+        console.log(error);
+        this.setState({error: true});
       });
+
     }
 
     postSelectedArgument = (id) => {
@@ -31,13 +37,17 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post =>{
-          return <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedArgument(post.id)} />;
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if(!this.state.error) {
+          posts = this.state.posts.map(post =>{
+            return <Post
+              key={post.id}
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedArgument(post.id)} />;
+          });
+        }
+
         return (
             <div>
                 <section className="Posts">
